afa089d83740edfb818ad6c4866802afbc7e8389
Jonnvoo <Jonathanvu2065@gmail.com>
2022-11-09 14:53:40 -0800

    added front-end routes and added views handlebars

--- controllers/usersRoutes.js
+++ controllers/usersRoutes.js
@@ -4,7 +4,11 @@ const router =express.Router();
 
 router.get('/', async (req, res) => {
     res.render("home")
-})
+});
+
+router.get('/sign-up', async (req, res) => {
+    res.render("Sign-up")
+});
  
 router.get("/sessions", (req,res) =>{ 
     res.json(req.session)
@@ -13,11 +17,11 @@ router.get("/sessions", (req,res) =>{
  
 router.get("/profile", (req,res) =>{
     if(!req.session.logged_id){
-        return res.redirect("home")
+        return res.redirect("/home")
     }
     User.findByPk(req.session.user_id)
     res.json("profile")
-})
+});
 
 
 // router.get('/', async (req, res) => {
@@ -31,32 +35,38 @@ router.get("/profile", (req,res) =>{
 
 
 
-// router.post('/login' , async (req, res) => { 
-//     try {
-//         const loginData = await Users.findByPk({ where: { username :req.body.username}});
-//         if (!loginData) {
-//             res
-//             .status(400)
-//             .json({ message: "Incorrect username or password"})
-//             return;
-//         }
-//         const passwordCheck = await loginData.checkPassword(req.body.password);
-//         if (!loginData) {
-//             res
-//                 .status(400)
-//                 .json({ message: "Incorrect username or password"})
-//                 return;
-//         }
 
-//         req.session.save(() => {
-//             req.session.user_id = loginData.id;
-//             req.session.logged_in = true;
+router.get('/sign-up', async (req, res) => {
+    res.render("Sign-up")
+});
+
+
+router.post('/login' , async (req, res) => { 
+    try {
+        const loginData = await Users.findByPk({ where: { username :req.body.username}});
+        if (!loginData) {
+            res
+            .status(400)
+            .json({ message: "Incorrect username or password"})
+            return;
+        }
+        const passwordCheck = await loginData.checkPassword(req.body.password);
+        if (!loginData) {
+            res
+                .status(400)
+                .json({ message: "Incorrect username or password"})
+                return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = loginData.id;
+            req.session.logged_in = true;
             
-//             res.json({user : loginData, message: "log in succesful"});
-//         });
-//         } catch (err) {
-//             res.status(400).json(err);
-//           }
-//         });
+            res.json({user : loginData, message: "log in succesful"});
+        });
+        } catch (err) {
+            res.status(400).json(err);
+          }
+        });
 
 module.exports = router;
