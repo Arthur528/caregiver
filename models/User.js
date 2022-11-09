aca1dabbeef752212547bb02c15b6f96c278323a
Jonnvoo <Jonathanvu2065@gmail.com>
2022-11-09 15:00:36 -0800

    add front-end routes and added views handlebars

--- models/User.js
+++ models/User.js
@@ -1,94 +1,94 @@
-// const { Model, DataTypes } = require('sequelize');
-// const bcrypt = require('bcrypt');
-// const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-// class User extends Model {
-//   checkPassword(loginPw) {
-//     return bcrypt.compareSync(loginPw, this.password);
-//   }
-// }
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-// User.init(
-//   {
-//     NurseId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       validate: {
-//         len: [10],
-//       },
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//       validate: {
-//         isEmail: true,
-//       },
-//     },
-//     password: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       validate: {
-//         len: [8],
-//       },
-//     },
-//     phone_number: {
-//       type: DataTypes.INTEGER,
-//     },
-//     bio: {
-//       type: DataTypes.STRING,
-//     },
-//     referral: {
-//       type: DataTypes.STRING,
-//     },
-//     is_driving: {
-//       type: DataTypes.BOOLEAN,
-//       defaultValue: false,
-//     },
-//     shift_info: {
-//       type: DataTypes.STRING,
-//     },
-//     street_address: {
-//       type: DataTypes.STRING,
-//     },
-//     city: {
-//       type: DataTypes.STRING,
-//     },
-//     zip_code: {
-//       type: DataTypes.STRING,
-//     },
-//     license_plate: {
-//       type: DataTypes.STRING,
-//     },
-//     car_model: {
-//       type: DataTypes.STRING,
-//     },
-//     car_color: {
-//       type: DataTypes.STRING,
-//     },
-//   },
-//   {
-//     hooks: {
-//       beforeCreate: async (newUserData) => {
-//         newUserData.password = await bcrypt.hash(newUserData.password, 10);
-//         return newUserData;
-//       },
-//       beforeUpdate: async (updatedUserData) => {
-//         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-//         return updatedUserData;
-//       },
-//     },
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'user',
-//   }
-// );
+User.init(
+  {
+    NurseId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      validate: {
+        len: [10],
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    phone_number: {
+      type: DataTypes.INTEGER,
+    },
+    bio: {
+      type: DataTypes.STRING,
+    },
+    referral: {
+      type: DataTypes.STRING,
+    },
+    is_driving: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    shift_info: {
+      type: DataTypes.STRING,
+    },
+    street_address: {
+      type: DataTypes.STRING,
+    },
+    city: {
+      type: DataTypes.STRING,
+    },
+    zip_code: {
+      type: DataTypes.STRING,
+    },
+    license_plate: {
+      type: DataTypes.STRING,
+    },
+    car_model: {
+      type: DataTypes.STRING,
+    },
+    car_color: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
 
-// module.exports = User;
+module.exports = User;
