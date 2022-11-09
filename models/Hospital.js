fa5f1d5ae837459396b3202df453d7918ddfe14c
James Pace <pacej2022@gmail.com>
2022-11-09 15:06:19 -0800

    updating model

--- models/Hospital.js
+++ models/Hospital.js
@@ -1,44 +1,33 @@
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// class Hospital extends Model {}
+class Hospital extends Model {}
 
-// Hospital.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     user_id: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: "user",
-//         key: "id",
-//       },
-//     },
-//     description: {
-//       type: DataTypes.STRING,
-//     },
-//     blog_id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       defaultValue: DataTypes.NOW,
-//     },
-//     date_created: {
-//       type: DataTypes.DATE,
-//       allowNull: false,
-//       defaultValue: DataTypes.NOW,
-//     },
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'hospital',
-//   }
-// );
+Hospital.init(
+  {
+    hospital_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        street_address: {
+          type: DataTypes.STRING,
+        },
+        city: {
+          type: DataTypes.STRING,
+        },
+        zip_code: {
+          type: DataTypes.STRING,
+        },
+  },
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'hospital',
+  }
 
-// module.exports = Hospital;
+
+);
+
+module.exports = Hospital;
