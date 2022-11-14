5a12ec99709d0be2e1fac25c8fb04533b03fed04
Jonathan Cornet <cornetj2@gmail.com>
2022-11-13 22:49:30 -0800

    fixed a bug in controllers.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const usersRoutes = require("./usersRoutes");
-router.use("/api/users", usersRoutes)
-const FrontendRoutes = require("./frontendRoutes")
-router.use("/",FrontendRoutes)
+router.use("/api/users", usersRoutes);
+const FrontendRoutes = require("./FrontendRoutes");
+router.use("/", FrontendRoutes);
 
 router.get("/session" , (req,res) => {
     res.json(req.session);
