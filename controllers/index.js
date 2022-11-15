a4c9c6b902b65b55c9648dda5ebd513d1cc5c853
Jonnvoo <Jonathanvu2065@gmail.com>
2022-11-14 23:38:28 -0800

    fix merge

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const usersRoutes = require("./usersRoutes");
 router.use("/api/users", usersRoutes);
-const frontendRoutes = require("./FrontendRoutes");
+const frontendRoutes = require("./frontendRoutes");
 router.use("/", frontendRoutes);
 
 router.get("/session" , (req,res) => {
